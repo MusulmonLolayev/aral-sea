a09fd3bce2e888b9d5b6b359a2ee884d30ff5161
MusulmonLolayev <musulmon.lolayev.94@gmail.com>
2021-03-05 15:16:12 +0500

    The general notify and dialog were removed.

--- frontend/src/store/common/actions.js
+++ frontend/src/store/common/actions.js
@@ -1,6 +1,3 @@
-import { Notify } from 'quasar'
-import { Dialog } from 'quasar'
-
 export function setIsRefreshingTokenExpired(context, { status }) {
     context.commit('setIsRefreshingTokenExpired', { status: status })
 }
@@ -9,22 +6,4 @@ export function setLangOptions(context) {
 }
 export function setConnectionStatus(context, { status }) {
     context.commit('setConnectionStatus', { status: status })
-}
-
-export function no_internet_notify(context) {
-    Notify.create({
-        message: this.lang.t('no_internet_connection'),
-        color: "red",
-        icon: "error",
-        actions: [{ label: this.lang.t("close"), color: "white" }],
-    })
-}
-
-export function internal_server_error_dialog(context) {
-    Dialog.create({
-        title: null,
-        message: this.lang.t('internal_server_error'),
-        ok: true,
-        persistent: true,
-    })
 }
