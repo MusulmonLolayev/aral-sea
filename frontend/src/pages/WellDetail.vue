b22e132543a9dc5711ed5e62891379abf112d742
MusulmonLolayev <musulmon.lolayev.94@gmail.com>
2021-03-05 15:15:34 +0500

    The Detail,vue name was renamed.

---
+++ frontend/src/pages/WellDetail.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <q-list bordered class="rounded-borders">
+      <div class="row">
+        <div class="col-5" style="margin: 20px">
+          <strong>{{title}}</strong>
+        </div>
+      </div>
+      <q-expansion-item
+        expand-separator
+        default-opened
+        icon="live_help"
+        :label="$t('musters')"
+        dense
+      >
+        <div style="margin: 10px">
+          <muster-table :well_id="$route.params.id" />
+        </div>
+      </q-expansion-item>
+    </q-list>
+  </div>
+</template>
+
+<script>
+import MusterTable from "../components/list/muster-table";
+export default {
+  data() {
+    return {
+      title: "",
+      well: {},
+    };
+  },
+
+  methods: {
+    initialize() {
+      if (this.$store.state.well.well != null) {
+        this.well = this.$store.state.well.well;
+        this.title = this.well.number + " " + this.well.farm_name;
+      } else {
+        this.$axios
+          .get("/well_request/" + this.$route.params.id)
+          .then((response) => {
+            this.well = response.data[0];
+            this.title = this.well.number + " " + this.well.farm_name;
+          });
+      }
+    },
+  },
+
+  beforeMount() {
+    //this.initialize();
+  },
+  mounted() {
+    this.initialize();
+  },
+  meta() {
+    return {
+      title: this.title,
+    };
+  },
+
+  components: { MusterTable },
+};
+</script>
+
+<style>
+</style>
