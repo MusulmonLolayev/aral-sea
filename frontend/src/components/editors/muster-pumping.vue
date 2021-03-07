1685ed5efe1863ca4f620641dfbf77084e2c0316
MusulmonLolayev <musulmon.lolayev.94@gmail.com>
2021-03-07 17:28:53 +0500

    The odd div was removed.

--- frontend/src/components/editors/muster-pumping.vue
+++ frontend/src/components/editors/muster-pumping.vue
@@ -1,104 +1,102 @@
 <template>
   <q-form ref="form">
-    <div>
-      <div class="row">
-        <div class="col-5">
-          <q-input
-            v-model="muster_pumping.starting_pumping"
-            :label="$t('starting_pumping')"
-            type="time"
-            style="margin-right: 30px"
-            :rules="[() => required('starting_pumping')]"
-          />
-        </div>
-        <div class="col-5">
-          <q-input
-            v-model="muster_pumping.finishing_pumping"
-            :label="$t('finishing_pumping')"
-            type="time"
-            style="margin-right: 30px"
-            :rules="[() => required('finishing_pumping')]"
-          />
-        </div>
-        <div class="col-5">
-          <q-input
-            v-model="muster_pumping.count_gall"
-            :label="$t('count_gall')"
-            type="number"
-            style="margin-right: 30px"
-            :rules="[() => required('count_gall')]"
-          />
-        </div>
-        <div class="col-5">
-          <q-input
-            v-model="muster_pumping.size_gall"
-            :label="$t('size_gall')"
-            type="number"
-            style="margin-right: 30px"
-            :rules="[() => required('size_gall')]"
-          />
-        </div>
-        <div class="col-5">
-          <q-input
-            v-model="muster_pumping.ugv_before_pumping"
-            :label="$t('ugv_before_pumping')"
-            type="number"
-            style="margin-right: 30px"
-            :rules="[() => required('ugv_before_pumping')]"
-          />
-        </div>
-        <div class="col-5">
-          <q-input
-            v-model="muster_pumping.ugv_after_pumping"
-            :label="$t('ugv_after_pumping')"
-            type="number"
-            style="margin-right: 30px"
-            :rules="[() => required('ugv_after_pumping')]"
-          />
-        </div>
-        <div class="col-5">
-          <q-input
-            v-model="muster_pumping.bottom"
-            :label="$t('bottom')"
-            type="number"
-            style="margin-right: 30px"
-            :rules="[() => required('bottom')]"
-          />
-        </div>
-        <div class="col-5">
-          <q-input
-            v-model="muster_pumping.speed_water"
-            :label="$t('speed_water')"
-            type="number"
-            style="margin-right: 30px"
-            :rules="[() => required('speed_water')]"
-          />
-        </div>
-        <div class="col-5">
-          <q-input
-            v-model="muster_pumping.elevated"
-            :label="$t('elevated')"
-            type="number"
-            style="margin-right: 30px"
-            :rules="[() => required('elevated')]"
-          />
-        </div>
-        <div class="col-5">
-          <q-input
-            v-model="muster_pumping.reduced"
-            :label="$t('reduced')"
-            type="number"
-            style="margin-right: 30px"
-            :rules="[() => required('reduced')]"
-          />
-        </div>
-        <div class="col-5">
-          <q-input
-            v-model="muster_pumping.date"
-            :label="$t('date')"
-            type="date"
-          />
-        </div>
+    <div class="row">
+      <div class="col-5">
+        <q-input
+          v-model="muster_pumping.starting_pumping"
+          :label="$t('starting_pumping')"
+          type="time"
+          style="margin-right: 30px"
+          :rules="[() => required('starting_pumping')]"
+        />
+      </div>
+      <div class="col-5">
+        <q-input
+          v-model="muster_pumping.finishing_pumping"
+          :label="$t('finishing_pumping')"
+          type="time"
+          style="margin-right: 30px"
+          :rules="[() => required('finishing_pumping')]"
+        />
+      </div>
+      <div class="col-5">
+        <q-input
+          v-model="muster_pumping.count_gall"
+          :label="$t('count_gall')"
+          type="number"
+          style="margin-right: 30px"
+          :rules="[() => required('count_gall')]"
+        />
+      </div>
+      <div class="col-5">
+        <q-input
+          v-model="muster_pumping.size_gall"
+          :label="$t('size_gall')"
+          type="number"
+          style="margin-right: 30px"
+          :rules="[() => required('size_gall')]"
+        />
+      </div>
+      <div class="col-5">
+        <q-input
+          v-model="muster_pumping.ugv_before_pumping"
+          :label="$t('ugv_before_pumping')"
+          type="number"
+          style="margin-right: 30px"
+          :rules="[() => required('ugv_before_pumping')]"
+        />
+      </div>
+      <div class="col-5">
+        <q-input
+          v-model="muster_pumping.ugv_after_pumping"
+          :label="$t('ugv_after_pumping')"
+          type="number"
+          style="margin-right: 30px"
+          :rules="[() => required('ugv_after_pumping')]"
+        />
+      </div>
+      <div class="col-5">
+        <q-input
+          v-model="muster_pumping.bottom"
+          :label="$t('bottom')"
+          type="number"
+          style="margin-right: 30px"
+          :rules="[() => required('bottom')]"
+        />
+      </div>
+      <div class="col-5">
+        <q-input
+          v-model="muster_pumping.speed_water"
+          :label="$t('speed_water')"
+          type="number"
+          style="margin-right: 30px"
+          :rules="[() => required('speed_water')]"
+        />
+      </div>
+      <div class="col-5">
+        <q-input
+          v-model="muster_pumping.elevated"
+          :label="$t('elevated')"
+          type="number"
+          style="margin-right: 30px"
+          :rules="[() => required('elevated')]"
+        />
+      </div>
+      <div class="col-5">
+        <q-input
+          v-model="muster_pumping.reduced"
+          :label="$t('reduced')"
+          type="number"
+          style="margin-right: 30px"
+          :rules="[() => required('reduced')]"
+        />
+      </div>
+      <div class="col-5">
+        <q-input
+          v-model="muster_pumping.date"
+          :label="$t('date')"
+          type="date"
+        />
       </div>
     </div>
   </q-form>
