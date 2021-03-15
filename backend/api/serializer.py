337e2f28b42acff5e7418964d6e75bce2e120304
MusulmonLolayev <musulmon.lolayev.94@gmail.com>
2021-03-15 07:01:23 +0500

    The staff serializer was added.

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -42,3 +42,9 @@ class MusterPumpingSerializer(serializers.ModelSerializer):
     class Meta:
         model = MusterPumping
         fields = '__all__'
+
+class StaffSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Staff
+        fields = '__all__'
